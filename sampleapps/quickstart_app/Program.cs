6567c944207cb441eab6a4d2bc125c18a5712a67
Eric Newton <ericnewton76@gmail.com>
2018-01-13 22:39:20 -0500

    added samples outlined in the README#quickstarts

---
+++ sampleapps/quickstart_app/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+
+namespace quickstart_app
+{
+	class Program
+	{
+		class Options
+		{
+			[Option('r', "read", Required = true, HelpText = "Input files to be processed.")]
+			public IEnumerable<string> InputFiles { get; set; }
+
+			// Omitting long name, defaults to name of property, ie "--verbose"
+			[Option(Default = false, HelpText = "Prints all messages to standard output.")]
+			public bool Verbose { get; set; }
+
+			[Option("stdin", Default = false, HelpText = "Read from stdin")]
+			public bool stdin { get; set; }
+
+			[Value(0, MetaName = "offset", HelpText = "File offset.")]
+			public long? Offset { get; set; }
+		}
+
+		static void Main(string[] args)
+		{
+			CommandLine.Parser.Default.ParseArguments<Options>(args)
+			  .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
+			  .WithNotParsed<Options>((errs) => HandleParseError(errs));
+		}
+
+		static int RunOptionsAndReturnExitCode(Options options)
+		{
+
+			return 0;
+		}
+
+		static void HandleParseError(IEnumerable<Error> errs)
+		{
+			
+		}
+
+	}
+}
---
+++ sampleapps/quickstart_verbs/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+
+namespace quickstart_verbs
+{
+	[Verb("add", HelpText = "Add file contents to the index.")]
+	class AddOptions
+	{
+		//normal options here
+	}
+	[Verb("commit", HelpText = "Record changes to the repository.")]
+	class CommitOptions
+	{
+		//normal options here
+	}
+	[Verb("clone", HelpText = "Clone a repository into a new directory.")]
+	class CloneOptions
+	{
+		//normal options here
+	}
+
+
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			return CommandLine.Parser.Default.ParseArguments<AddOptions, CommitOptions, CloneOptions>(args)
+			  .MapResult(
+				(AddOptions opts) => RunAddAndReturnExitCode(opts),
+				(CommitOptions opts) => RunCommitAndReturnExitCode(opts),
+				(CloneOptions opts) => RunCloneAndReturnExitCode(opts),
+				errs => 1);
+		}
+
+		static int RunAddAndReturnExitCode(AddOptions opts)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static int RunCommitAndReturnExitCode(CommitOptions opts)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static int RunCloneAndReturnExitCode(CloneOptions opts)
+		{
+			throw new NotImplementedException();
+		}
+
+
+	}
+}
